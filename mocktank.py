3cdc2a9a67d089f930678b9582d57bbf0b53055f
Tony Forgan <tonyforgan@gmail.com>
2014-03-26 17:32:29 -0400

    More work on events

--- mocktank.py
+++ mocktank.py
@@ -8,12 +8,15 @@ from mocksensor import MockSensor
 class MockTank:
     def __init__(self):
         self._upper = MockSensor(False)
-        self._lower = MockSensor(False)
+        #self._lower = MockSensor(False)
+        self._upper.onstatechanged += statechanged
 
-        self._tank = Tank(self._upper, self._lower)
-        self._pump = MockPump()
+        #self._tank = Tank(self._upper, self._lower)
+        #self._pump = MockPump()
 
     def run(self):
-        self._pump.empty(self._tank)
+        self._upper.activate()
+        raw_input("Press enter to end")
 
-        print(self._tank.isempty)
+    def statechanged(self, currentstate):
+        print "Seconds = %d." % currentstate
